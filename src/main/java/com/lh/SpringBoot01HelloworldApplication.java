b994c66afc8dcd524bc1b6a13b67a74ee2e4ea5a
huanliu7 <279851173@qq.com>
2019-06-26 20:11:01 +0800

    helloworld

---
+++ src/main/java/com/lh/SpringBoot01HelloworldApplication.java
@@ -0,0 +1,13 @@
+package com.lh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot01HelloworldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot01HelloworldApplication.class, args);
+    }
+
+}
