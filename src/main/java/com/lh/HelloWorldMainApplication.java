e99c8a3099d8e1f94ef688b4e44a709b957c5458
huanliu7 <279851173@qq.com>
2019-06-27 10:16:45 +0800

    helloworld快速启动

--- src/main/java/com/lh/HelloWorldMainApplication.java
+++ src/main/java/com/lh/HelloWorldMainApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBoot01HelloworldApplication {
+public class HelloWorldMainApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBoot01HelloworldApplication.class, args);
+        SpringApplication.run(HelloWorldMainApplication.class, args);
     }
 
 }
